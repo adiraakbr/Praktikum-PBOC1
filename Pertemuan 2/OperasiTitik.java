2b7dba203077a7f1dcc46b723c20b42c6aae6797
adiraakbr <100354437+adiraakbr@users.noreply.github.com>
2023-03-05 22:51:13 +0700

    Update OperasiTitik.java

--- Pertemuan 2/OperasiTitik.java
+++ Pertemuan 2/OperasiTitik.java
@@ -9,17 +9,26 @@
 
 
 class OperasiTitik {
-    public void refleksiSumbuX(Titik titik) {
+    private void refleksiSumbuX(Titik titik) {
         double y;
-		y = titik.getOrdinat();
-		y *= -1;
-		titik.setOrdinat(y);
+	   y = titik.getOrdinat();
+	   y *= -1;
+	   titik.setOrdinat(y);
     }
 
-    public void refleksiSumbuY(Titik titik) {
+    private void refleksiSumbuY(Titik titik) {
         double x;
-		x = titik.getAbsis();
-		x *= -1;
-		titik.setAbsis(x);
+	   x = titik.getAbsis();
+	   x *= -1;
+	   titik.setAbsis(x);
+    }
+    public Titik refleksiX(Titik titik) {
+	   refleksiSumbuX(titik);
+	   return titik;
+    }
+
+    public Titik refleksiY(Titik titik) {
+	   refleksiSumbuY(titik);
+	   return titik;
     }
 }
