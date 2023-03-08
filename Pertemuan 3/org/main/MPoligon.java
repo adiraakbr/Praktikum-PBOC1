3579821db17c18cfee988c32ccb91f814ffe8f56
adiraakbr <100354437+adiraakbr@users.noreply.github.com>
2023-03-08 19:28:31 +0700

    Add files via upload

---
+++ Pertemuan 3/org/main/MPoligon.java
@@ -0,0 +1,26 @@
+/**
+ * File : MPoligon.java 08/03/23
+ * Penulis : Adira Rahmana Akbar
+ * Deskripsi : Driver class untuk poligon dan persegi panjang
+ *
+ */
+
+package org.main;
+
+import org.bangundatar.*;
+
+/*public class MPoligon{
+    public static void main(String[] args){
+        PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+        persegi.printInfo();
+        System.out.println("Luas Persegi Panjang: " + persegi.hitungLuas());
+    }
+}*/
+
+public class MPoligon{
+    public static void main(String[] args){
+        Segitiga segitiga = new Segitiga(10,10,3);
+        segitiga.printInfo();
+        System.out.println("Luas Segitiga: " + segitiga.hitungLuas());
+    }
+}
